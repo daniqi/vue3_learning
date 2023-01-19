b486587ad615150c7483e46292da8eebd8c4e257
Vincent Halman <86776071+vincenthalman@users.noreply.github.com>
2023-01-19 16:10:15 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Kijk ook eens naar Nuxt3. Werkt wel nice met Vue3"/>
 </template>
 
 <script>
