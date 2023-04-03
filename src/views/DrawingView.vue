22ca0058b22c3c488b458437f5974f9abd3d2983
Danick Sikkema <danick.sikkema@gmail.com>
2023-04-03 21:25:36 +0200

    on file input draw the image on canvas

--- src/views/DrawingView.vue
+++ src/views/DrawingView.vue
@@ -5,11 +5,33 @@
                 <canvas width="500" height="200" @mousedown="onMouseDown" @mouseup="onMouseUp" @mousemove="onMouseMove" id="drawing-board"></canvas>
             </v-col>
         </v-row>
+
+        <v-file-input 
+            label="File input"
+            v-model="files"
+            placeholder="Upload your documents"
+            multiple
+            prepend-icon="mdi-paperclip"
+            @change="loadFiles"
+            >
+            <template v-slot:selection="{ fileNames }">
+                <template v-for="fileName in fileNames" :key="fileName">
+                    <v-chip
+                    size="small"
+                    label
+                    color="primary"
+                    class="me-2"
+                    >
+                    {{ fileName }}
+                    </v-chip>
+                </template>
+            </template>
+        </v-file-input>
     </v-container>
 </template>
 
 <script setup>
-    import { onMounted } from 'vue'
+    import { ref, onMounted } from 'vue'
 
     let isPainting = false
 
@@ -20,6 +42,8 @@
     let canvasOffsetY = 0
 
     let lineWidth = 10;
+
+    let files = ref([])
 
     function onMouseDown(e) {
         isPainting = true;
@@ -47,7 +71,6 @@
 
     onMounted(() => {
         canvas = document.getElementById('drawing-board');
-
         context = canvas.getContext('2d');
 
         // offsets (distance between the canvas edges to the edge of the viewport)
@@ -57,6 +80,48 @@
         canvas.width = window.innerWidth - canvasOffsetX;
         canvas.height = window.innerHeight - canvasOffsetY;
     })
+
+    async function drawOnImage(file = null) {
+        // if an image is present,
+        // the image passed as a parameter is drawn in the canvas
+        if (file) {
+            
+            let image = new Image();
+            image.src = await fileToDataUri(file);
+
+            const imageWidth = image.width;
+            const imageHeight = image.height;
+
+            // rescaling the canvas element
+            // canvas.width = imageWidth;
+            // canvas.height = imageHeight;
+
+            image.onload = function() {
+                context.drawImage(image, 0 ,0);
+            };
+
+            context.drawImage(image, 0, 0, imageWidth, imageHeight);
+        }
+    }
+
+    function loadFiles(event) {
+        // TODO: make single file
+        const [file] = event.target.files;
+
+        drawOnImage(file)
+    }
+
+    function fileToDataUri(field) {
+        return new Promise((resolve) => {
+            const reader = new FileReader();
+
+            reader.addEventListener("load", () => {
+            resolve(reader.result);
+        });
+
+        reader.readAsDataURL(field);
+    });
+}
 </script>
 
 <style>
